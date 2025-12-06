d8f0d8a4518dac44473a091e66101865c81f9fc0
orc44l <orc44l@gmail.com>
2025-12-06 12:43:36 +0100

    challenge jour 5 - Greg

--- challenge 10/greg.php
+++ challenge 10/greg.php
@@ -3,9 +3,6 @@
 $handle = fopen("input.txt", "r");
 $validRangeList = [];
 $validIdCount = 0;
-$maxId = 0;
-$minId = PHP_INT_MAX;
-
 $result = 0;
 
 if ($handle) {
@@ -46,101 +43,7 @@ if ($handle) {
         
         $maxId = $currentRange[1] + 1;
     }
-    // $mergedRangeList = [];
-    /*
-    foreach ($validRangeList as $originalRange) // A - B
-    {
-        // var_dump(__LINE__);
-        $A = $originalRange[0];
-        $B = $originalRange[1];
-        foreach ($mergedRangeList as &$currentMerged) // c - d
-        {
-            $c = $currentMerged[0];
-            $d = $currentMerged[1];
-            if ($A < $c) // A - c
-            {
-                // A - cd - B => AB
-                if ($B > $d)
-                {
-                    $currentMerged[0] = $A;
-                    $currentMerged[1] = $B;
-                }
-                // A - c - B - d => Ad
-                elseif ($B >= $c - 1)
-                {
-                    $currentMerged[0] = $A;
-                }
-                // AB cd
-                elseif ($B < $c)
-                {
-                    array_unshift($mergedRangeList, $originalRange);
-                }
-                continue 2;
-            }
-            elseif ($A < $d)
-            {
-                // c - A - d - B => cB
-                if ($B > $d)
-                {
-                    $currentMerged[1] = $B;
-                }
-                continue 2;
-            }
-        }
-        array_push($mergedRangeList, $originalRange);
-    }
-    */
-    // echo __LINE__ . ' / ' . count($mergedRangeList) . PHP_EOL;
-    // print_r($mergedRangeList);
-
-    // foreach ($mergedRangeList as $currentMerged)
-    // {
-    //     echo $currentMerged[0] . ' - ' . $currentMerged[1]  . PHP_EOL;
-    //     $validIdCount += ($currentMerged[1] - $currentMerged[0] + 1);
-    // }
-
-    // $finalRangeList = $validRangeList;
-    // do {
-    //     $tempList = $finalRangeList;
-    //     $finalRangeList = mergeRangeList($tempList);
-
-    // } while(count($finalRangeList) != count($tempList));
-
-    // echo '--------------' . PHP_EOL;
-    // foreach ($finalRangeList as $key => $currentMerged)
-    // {
-    //     echo $currentMerged[0] . ' - ' . $currentMerged[1]  . PHP_EOL;
-    //     $validIdCount += ($currentMerged[1] - $currentMerged[0] + 1);
-
-    //     // if ($key > 10) break;
-    // }
 
     echo 'resultat : ' . $result . PHP_EOL;
     echo 'nombre id valid : ' . $validIdCount . PHP_EOL;
 }
-
-// function mergeRangeList($rangeList)
-// {
-//     $tempMergedList = [];
-//     for ($i = 0; $i < count($rangeList) - 1; $i++)
-//     {
-//         $currentRange = $rangeList[$i];
-//         $nextRange = $rangeList[$i+1];
-
-//         if ($currentRange[1] >= $nextRange[1])
-//         {
-//             $tempMergedList[] = $currentRange;
-//             $i++;
-//         }
-//         elseif ($currentRange[1] >= $nextRange[0] - 1)
-//         {
-//             $tempMergedList[] = [$currentRange[0], $nextRange[1]];
-//             $i++;
-//         }
-//         else
-//         {
-//             $tempMergedList[] = $currentRange;
-//         }
-//     }
-//     return $tempMergedList;
-// }
